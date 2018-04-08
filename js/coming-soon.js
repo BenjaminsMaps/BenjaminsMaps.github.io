257a2341ea86bc8f9ec9a6035980211f8359c4de
Ben Hazlerig <ben.hazlerig@uky.edu>
2018-04-08 10:20:19 -0400

    Updated portfolio page

---
+++ js/coming-soon.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Vide - Video Background Settings
+  $('body').vide({
+    mp4: "mp4/Cloud_Surf.mp4",
+    poster: "img/Cloud_Surf.jpg"
+  }, {
+    posterType: 'jpg'
+  });
+
+})(jQuery); // End of use strict
